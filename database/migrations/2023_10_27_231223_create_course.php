16445f134838d84120617a1721bed0f1c115cbdf
ekysambora <ekysambora@gmail.com>
2023-10-28 06:20:08 +0700

    feat: migrate table course

---
+++ database/migrations/2023_10_27_231223_create_course.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCourse extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('course', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->boolean("certificate");
+            $table->string("thumbnail");
+            $table->enum("type", ["free", "premium"]);
+            $table->enum("status", ["draft", "published"]);
+            $table->integer("price")->default(0)->nullable();
+            $table->enum("level", ["all-level", "beginner", "intermediet", "advance"]);
+            $table->longText("description")->nullable();
+            $table->foreignId("mentor_id")->constrained("mentors")->onDelete("cascade");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('course');
+    }
+}
