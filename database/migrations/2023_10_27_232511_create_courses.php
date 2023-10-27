3e29293777acb6c3ad151e6930cf42f57f2f9287
ekysambora <ekysambora@gmail.com>
2023-10-28 06:26:48 +0700

    fix: change name of table course

--- database/migrations/2023_10_27_232511_create_courses.php
+++ database/migrations/2023_10_27_232511_create_courses.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCourse extends Migration
+class CreateCourses extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCourse extends Migration
      */
     public function up()
     {
-        Schema::create('course', function (Blueprint $table) {
+        Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string("name");
             $table->boolean("certificate");
@@ -36,6 +36,6 @@ class CreateCourse extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('course');
+        Schema::dropIfExists('courses');
     }
 }
