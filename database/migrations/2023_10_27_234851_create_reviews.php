ba2c29574c28baee977db5796e6e786cc4b03188
ekysambora <ekysambora@gmail.com>
2023-11-19 20:52:28 +0700

    dummy push

--- database/migrations/2023_10_27_234851_create_reviews.php
+++ database/migrations/2023_10_27_234851_create_reviews.php
@@ -14,13 +14,13 @@ class CreateReviews extends Migration
     public function up()
     {
         Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
-            $table->integer("user_id");
-            $table->foreignId("course_id")->constrained("courses")->onDelete("cascade");
-            $table->integer("rating")->default(1);
-            $table->longText("note")->nullable();
-            $table->unique(["course_id", "user_id"]);
-            $table->timestamps();
+            // $table->id();
+            // $table->integer("user_id");
+            // $table->foreignId("course_id")->constrained("courses")->onDelete("cascade");
+            // $table->integer("rating")->default(1);
+            // $table->longText("note")->nullable();
+            // $table->unique(["course_id", "user_id"]);
+            // $table->timestamps();
         });
     }
 
